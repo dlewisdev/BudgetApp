ad198f1cc7c2cdb76df463a5dcdc93c9e2512c08
Danielle Lewis <msdaniellelewis@icloud.com>
2023-10-03 21:24:06 -0400

    Initial Commit

---
+++ Budget App/Budget_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_AppApp.swift
+//  Budget App
+//
+//  Created by Danielle Lewis on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Budget_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budget App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budget App
+//
+//  Created by Danielle Lewis on 10/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
