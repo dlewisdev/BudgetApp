c3090233959e143bb60455eee34d6d47e2569c0f
Danielle Lewis <msdaniellelewis@icloud.com>
2023-10-04 01:25:20 -0400

    Creates the category cards to be reused for each category on home screen

--- Budget App/CategoryCardView.swift
+++ Budget App/CategoryCardView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct CategoryCardView: View {
+    
+    @Binding var selectedCategory: Category
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                ZStack {
+                    Circle()
+                        .fill(selectedCategory.category.color)
+                        .frame(width: 50)
+                    Image("\(selectedCategory.category.rawValue)")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 25, height: 25)
+                        .padding()
+                }
+                .padding(.leading)
+                
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(selectedCategory.category.label)
+                        .font(.headline)
+                    Text("spent ") +
+                    Text("$\(selectedCategory.categoryTotalSpent) ")
+                        .foregroundStyle(.green) +
+                    Text("of ") +
+                    Text("$\(selectedCategory.categoryBudget)")
+                }
+                
+                
+                Spacer()
+                
+                VStack {
+                    Text("$\(selectedCategory.categoryAvailableBudget)")
+                        .font(.title3)
+                        .bold()
+                        .foregroundStyle(.green)
+                    Text("left")
+                }
+                .padding(.trailing)
+            }
+            
+            ProgressView(value: Double(selectedCategory.categoryTotalSpent),
+                         total: Double(selectedCategory.categoryBudget))
+            .progressViewStyle(LinearProgressViewStyle(tint: selectedCategory.category.color))
+        }
     }
 }
 
-#Preview {
-    CategoryCardView()
+
+struct CategoryCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        // Create a Category instance
+        let exampleCategory = Category(
+            category: .food,
+            categoryBudget: 100,
+            categoryTotalSpent: 20
+        )
+        
+        // Create a State instance from the example category
+        let selectedCategory = exampleCategory
+        
+        // Provide the binding to the CategoryCardView
+        CategoryCardView(selectedCategory: .constant(selectedCategory))
+    }
 }
